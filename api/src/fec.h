dc620950ee5b5e91084d0d5dde3b2308a5d02415
Wesley W. Terpstra <w.terpstra@gsi.de>
2011-03-30 09:47:49 +0000

    Prototype for FEC

--- api/src/fec.h
+++ api/src/fec.h
@@ -1,10 +1,13 @@
 #ifndef FEC_H
 #define FEC_H
 
+/* Initialize any buffers / state needed to encode/decode packets */
 void fec_open();
+
+/* Free any resources used by the encoder/decoder */
 void fec_close();
 
-/* Input: data received from network [chunk, chunk+*len)
+/* Input: data received from ethernet payload [chunk, chunk+*len)
  * Output:
  *   If cannot (yet) decode, returns 0
  *   Otherwise, returns pointer to decoded message and modifies *len
@@ -13,7 +16,7 @@ void fec_close();
  */
 unsigned char* fec_decode(unsigned char* chunk, unsigned int* len);
 
-/* Input: data to send [chunk, chunk+*len)
+/* Input: ethernet payload to send [chunk, chunk+*len)
  *        index, starts at 0 and incremented each call until done
  * Output:
  *   If no more chunks follow, returns 0
