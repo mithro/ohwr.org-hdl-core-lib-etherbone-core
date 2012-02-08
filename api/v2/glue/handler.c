d80f8652a64ec388a60b2a67f5deee706608547d
Wesley W. Terpstra <w.terpstra@gsi.de>
2012-02-08 17:48:46 +0000

    Split handler code into its own file.

---
+++ api/v2/glue/handler.c
@@ -0,0 +1,106 @@
+/** @file socket.c
+ *  @brief Implement the Etherbone socket data structure.
+ *
+ *  Copyright (C) 2011-2012 GSI Helmholtz Centre for Heavy Ion Research GmbH 
+ *
+ *  This data structure does not deal directly with IO.
+ *  For actual transport-layer sockets, see transport/.
+ *  For reading/writing of payload, see format/.
+ *
+ *  @author Wesley W. Terpstra <w.terpstra@gsi.de>
+ *
+ *  @bug None!
+ *
+ *******************************************************************************
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************
+ */
+
+#define ETHERBONE_IMPL
+
+#include "../memory/memory.h"
+#include "socket.h"
+#include "handler.h"
+
+eb_status_t eb_socket_attach(eb_socket_t socketp, eb_handler_t handler) {
+  eb_handler_address_t addressp, i;
+  eb_handler_callback_t callbackp;
+  struct eb_socket* socket;
+  struct eb_handler_address* address;
+  struct eb_handler_callback* callback;
+  
+  /* Get memory */
+  addressp = eb_new_handler_address();
+  if (addressp == EB_NULL)
+    return EB_OOM;
+  
+  callbackp = eb_new_handler_callback();
+  if (callbackp == EB_NULL) {
+    eb_free_handler_address(addressp);
+    return EB_OOM;
+  }
+  
+  socket = EB_SOCKET(socketp);
+  
+  /* See if it overlaps other devices */
+  for (i = socket->first_handler; i != EB_NULL; i = address->next) {
+    address = EB_HANDLER_ADDRESS(i);
+    if (((address->base ^ handler->base) & ~(address->mask | handler->mask)) == 0) {
+      eb_free_handler_callback(callbackp);
+      eb_free_handler_address(addressp);
+      return EB_ADDRESS;
+    }
+  }
+  
+  /* Insert the new virtual device */
+  address = EB_HANDLER_ADDRESS(addressp);
+  callback = EB_HANDLER_CALLBACK(callbackp);
+  
+  address->base = handler->base;
+  address->mask = handler->mask;
+  address->callback = callbackp;
+  callback->data = handler->data;
+  callback->read = handler->read;
+  callback->write = handler->write;
+  
+  address->next = socket->first_handler;
+  socket->first_handler = addressp;
+  return EB_OK;
+}
+
+eb_status_t eb_socket_detach(eb_socket_t socketp, eb_address_t target_address) {
+  eb_handler_address_t i, *ptr;
+  struct eb_socket* socket;
+  struct eb_handler_address* address;
+  
+  socket = EB_SOCKET(socketp);
+  
+  /* Find the device */
+  for (ptr = &socket->first_handler; (i = *ptr) != EB_NULL; ptr = &address->next) {
+    address = EB_HANDLER_ADDRESS(i);
+    if (address->base == target_address)
+      break;
+  }
+  
+  /* No device found? */
+  if (i == EB_NULL)
+    return EB_ADDRESS;
+  
+  /* Remove it */
+  *ptr = address->next;
+  eb_free_handler_callback(address->callback);
+  eb_free_handler_address(i);
+  return EB_OK;
+}
+
--- api/v2/glue/socket.h
+++ api/v2/glue/socket.h
@@ -32,28 +32,13 @@
 
 #include "../etherbone.h"
 #include "../transport/transport.h"
-
-typedef EB_POINTER(eb_handler_callback) eb_handler_callback_t;
-struct eb_handler_callback {
-  eb_user_data_t data;
-  
-  eb_status_t (*read) (eb_user_data_t, eb_address_t, eb_width_t, eb_data_t*);
-  eb_status_t (*write)(eb_user_data_t, eb_address_t, eb_width_t, eb_data_t);
-};
-
-typedef EB_POINTER(eb_handler_address) eb_handler_address_t;
-struct eb_handler_address {
-  eb_address_t base;
-  eb_address_t mask;
-  eb_handler_callback_t callback;
-  eb_handler_address_t next;
-};
+#include "handler.h"
 
 typedef EB_POINTER(eb_response) eb_response_t;
 struct eb_response {
   /* xxxxxxxxxxxxxxxL
-   * H=1 L=0 means read-back
-   * H=1 L=1 means status-back
+   * L=0 means read-back
+   * L=1 means status-back
    */
   uint16_t address;
   uint16_t deadline; /* Low 16-bits of a UTC seconds counter */
