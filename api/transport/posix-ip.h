ba72113ab4dab3474c176f9e6c0c0d79c3163405
Wesley W. Terpstra <w.terpstra@gsi.de>
2012-02-29 10:19:11 +0000

    getaddrinfo is not GNU specific. POSIX added it.

--- api/transport/posix-ip.h
+++ api/transport/posix-ip.h
@@ -37,7 +37,7 @@
 typedef int socklen_t;
 typedef SOCKET eb_posix_sock_t;
 #else
-#define _GNU_SOURCE
+#define _POSIX_C_SOURCE 1
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
