1982267eaba299d1fd5ddc3e3e5f64a00f027f8b
Mathias Kreider <m.kreider@gsi.de>
2012-10-02 10:26:08 +0200

    cleaned up folder

---
+++ hdl/eb_slave_core/Manifest.py
@@ -0,0 +1,14 @@
+files = [
+  "eb_main_fsm.vhd",
+  "eb_checksum.vhd",
+  "eb_config.vhd",
+  "eb_slave_core.vhd",
+  "eb_hdr_pkg.vhd",
+  "eb_rx_ctrl.vhd",
+  "eb_tx_ctrl.vhd",
+  "etherbone_pkg.vhd",
+  "piso_flag.vhd",
+  "sipo_flag.vhd",
+  "vhdl_2008_workaround_pkg.vhd",
+  "WB_bus_adapter_streaming_sg.vhd",
+  "xetherbone_core.vhd"];
