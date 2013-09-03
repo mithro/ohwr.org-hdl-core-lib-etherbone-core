39ba7f6f07924e6067ee7a431f8ee34e00e96d10
Mathias Kreider <m.kreider@gsi.de>
2013-09-03 13:30:21 +0200

    added missing Manifests for eb_master_core

--- Manifest.py
+++ Manifest.py
@@ -1,1 +1,1 @@
-modules = { "local": ["hdl/eb_slave_core", "hdl/eb_usb_core"] }
+modules = { "local": ["hdl/eb_master_core", "hdl/eb_slave_core", "hdl/eb_usb_core"] }
---
+++ hdl/eb_master_core/Manifest.py
@@ -0,0 +1,7 @@
+files = [
+  "eb_framer.vhd",
+  "eb_master_slave_wrapper.vhd",
+  "eb_master_top.vhd",
+  "eb_master_wb_if.vhd",
+  "eb_record_gen.vhd",
+  ]
