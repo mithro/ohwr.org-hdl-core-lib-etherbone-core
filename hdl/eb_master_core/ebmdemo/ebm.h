15478274e4e9f56b618b20b8793a826c9296702f
Mathias Kreider <m.kreider@gsi.de>
2013-09-06 17:10:25 +0200

    complete ebm api demo

--- hdl/eb_master_core/ebmdemo/ebm.h
+++ hdl/eb_master_core/ebmdemo/ebm.h
@@ -23,9 +23,8 @@
  *  License along with this library. If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************
  */
-
-#ifndef __EBM_H_
-#define __EBM_H_
+#ifndef EBM_H
+#define EBM_H
 
 
 #define EBM_REG_RESET         0                         
@@ -47,17 +46,42 @@
 #define EBM_REG_EB_OPT        (EBM_REG_ROA_BASE     +4) 
 #define EBM_REG_LAST          (EBM_REG_EB_OPT; 
 
+#define EBM_OFFS_DAT    0x00800000
+#define EBM_OFFS_WR 	  0x00400000
+#define EBM_OFFS_LOCAL  (EBM_REG_SRC_MAC_HI)
+#define EBM_OFFS_REMOTE (EBM_REG_DST_MAC_HI)
 
-#define EBM_OFFS_DAT  0x00800000
-#define EBM_OFFS_WR 	0x00400000
+#define EBM_OFFS_MAC_HI    0       
+#define EBM_OFFS_MAC_LO    (EBM_OFFS_MAC_HI   +4)    
+#define EBM_OFFS_IPV4      (EBM_OFFS_MAC_LO   +4)    
+#define EBM_OFFS_UDP_PORT  (EBM_OFFS_IPV4     +4)
 
-#define WRITE         (EBM_OFFS_WR)
-#define READ          0x00000000
-#define ADR_MASK      0x003FFFFF
+#define WRITE           (EBM_OFFS_WR)
+#define READ            0x00000000
+#define ADR_MASK        0x003FFFFF
 
+typedef struct eb_lm32_udp_link {
+  /* Contents must fit in 12 bytes */
+  unsigned char mac[6];
+  unsigned char ipv4[4];
+  unsigned short port;		
+};
+typedef unsigned int adress_type_t;
+typedef unsigned char target_t;
+
+static const target_t       LOCAL   = 0;
+static const target_t       REMOTE  = 1;
+static const adress_type_t  MAC     = 1;
+static const adress_type_t  IP      = 2;
+static const adress_type_t  PORT    = 3;
+static const unsigned short myPort  = 0xEBD0;
+
+
+
+void ebm_config_if(target_t conf, const char* con_info);
+void ebm_config_meta(unsigned int pac_len, unsigned int hi_bits, unsigned int max_ops, unsigned int eb_ops);
 void ebm_op(unsigned int address, unsigned int value, unsigned int optype);
 void ebm_flush(void);
-
 
 
 #endif
--- hdl/eb_master_core/ebmdemo/main.c
+++ hdl/eb_master_core/ebmdemo/main.c
@@ -3,9 +3,9 @@
 #include "irq.h"
 #include "ebm.h"
 
-volatile unsigned int* display = (unsigned int*)0x02900000;
-volatile unsigned int* irq_slave = (unsigned int*)0x02000d00;
-volatile unsigned int* ebm      = (unsigned int*)0x01000000;
+volatile unsigned int* display    = (unsigned int*)0x02900000;
+volatile unsigned int* irq_slave  = (unsigned int*)0x02000d00;
+volatile unsigned int* ebm        = (unsigned int*)0x01000000;
 
 
 char* mat_sprinthex(char* buffer, unsigned long val)
@@ -29,21 +29,17 @@ void show_msi()
 {
   char buffer[12];
   
-  mat_sprinthex(buffer, global_msi.msg);
+  
   disp_put_str("D ");
-  disp_put_str(buffer);
+  disp_put_str(mat_sprinthex(buffer, global_msi.msg));
   disp_put_c('\n');
 
-  
-  mat_sprinthex(buffer, global_msi.src);
   disp_put_str("A ");
-  disp_put_str(buffer);
+  disp_put_str(mat_sprinthex(buffer, global_msi.src));
   disp_put_c('\n');
 
-  
-  mat_sprinthex(buffer, (unsigned long)global_msi.sel);
   disp_put_str("S ");
-  disp_put_str(buffer);
+  disp_put_str(mat_sprinthex(buffer, (unsigned long)global_msi.sel));
   disp_put_c('\n');
 }
 
@@ -103,7 +99,11 @@ unsigned int time = 0;
 
 	char color = 0xFF;
 
-  
+  x = 0;
+	y = 9;
+	yinc = -1;
+ 	xinc = 1;
+	addr_raw_off = 0;
 
   disp_reset();	
   disp_put_c('\f');
@@ -114,18 +114,45 @@ unsigned int time = 0;
 
 
 
-	x = 0;
-	y = 9;
-	yinc = -1;
- 	xinc = 1;
-	addr_raw_off = 0;
+	
+	///////////////////////////////////////////////////////////////////////////////////
+	//Init EB Master
+	ebm_config_if(LOCAL,   "hw/08:00:30:e3:b0:5a/udp/192.168.191.254/port/60368");
+	ebm_config_if(REMOTE,  "hw/bc:30:5b:e2:b0:88/udp/192.168.191.131/port/60368");
+  ebm_config_meta(80, 0x11, 16, 0x00000000 );
 	
   while (1) {
-    /* Rotate the LEDs */
-    
+     //Send packet heartbeat
+     
+     if(time++ > 50) { 
+      //create WB cycle
+      ebm_op(0x55000333, 0xDEADBEEF, WRITE);
+      ebm_op(0x22000333, 0xCAFEBABE, READ);  
+      //send
+      ebm_flush(); 
+     } 
+  ///////////////////////////////////////////////////////////////////////////////////
 
 
-  disp_put_raw( get_pixcol_val((unsigned char)y), get_pixcol_addr((unsigned char)x, (unsigned char)y), color);
+
+
+	
+	
+
+      /* Each loop iteration takes 4 cycles.
+       * It runs at 125MHz.
+       * Sleep 0.2 second.
+       */
+      
+
+   
+
+
+//  disp_put_raw( get_pixcol_val((unsigned char)y), get_pixcol_addr((unsigned char)x, (unsigned char)y), color);
+
+for (j = 0; j < 62500000/160; ++j) {
+        asm("# noop"); /* no-op the compiler can't optimize away */
+              }
 
 
 	if(x == 63) xinc = -1;
@@ -136,22 +163,6 @@ unsigned int time = 0;
 
 	x += xinc;
 	y += yinc;
-
-
-	
-	
-
-      /* Each loop iteration takes 4 cycles.
-       * It runs at 125MHz.
-       * Sleep 0.2 second.
-       */
-      for (j = 0; j < 125000000/160; ++j) {
-        asm("# noop"); /* no-op the compiler can't optimize away */
-              }
-
-  if(time++ > 50) {
-  ebm_op(0x333, 0xDEADBEEF, WRITE); ebm_flush();
-  }    
 
 	if(time++ > 500) {time = 0; color = ~color; }
 	
